474a9c9c77860c16e31f02a55a5f47c930a6d55a
akulak <akulak@stud.khpi.ucode-connect.study>
2022-09-24 16:50:36 +0300

    aa

---
+++ inc/header.h
@@ -0,0 +1,20 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <ncurses.h>
+#include <unistd.h>
+#include <time.h>
+
+struct rain {
+    int color;
+    int length;
+    int speed;
+    int direction;
+    char symbol;
+}      rain;
+
+
+#endif
+
